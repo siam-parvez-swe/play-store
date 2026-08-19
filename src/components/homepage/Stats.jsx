bc4e77bac97b9d546df67bfb606bef166e9d2168
siamparvez001 <siamparvez003@gmail.com>
2026-08-20 00:33:27 +0600

    complete stats section

--- src/components/homepage/Stats.jsx
+++ src/components/homepage/Stats.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Stats = () => {
     return (
         <div>
-            Stats
+            <h1 className='text-5xl font-bold bg-purple-500 text-center py-15'>Stats</h1>
         </div>
     );
 };
